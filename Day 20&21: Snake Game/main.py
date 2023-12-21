f054b89d10c11526569e541a4ab5fdbf8a505e29
Ayman Haseeb <114000374+Aymmaann@users.noreply.github.com>
2023-12-21 20:29:47 +0530

    Added main game file

---
+++ Day 20&21: Snake Game/main.py
@@ -0,0 +1,53 @@
+from turtle import Turtle, Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Right", fun=snake.right)
+
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+    
+    # Collision with food
+    if snake.head.distance(food) < 18:
+        food.generate_food()
+        snake.extend()
+        scoreboard.update_score()
+
+    # Collision with wall
+    if snake.head.xcor() > 298 or snake.head.xcor() < -298 or snake.head.ycor() > 298 or snake.head.ycor() < -298:
+        game_on = False
+        scoreboard.game_over()
+
+    # Collision with itself
+    for segment in snake.snake_body[1:]:
+            if snake.head.distance(segment) < 17:
+                game_on = False
+                scoreboard.game_over()
+
+
+
+
+
+
+screen.exitonclick()
